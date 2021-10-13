d20675c0dbc3941c55c631b3d5d9b0360ff6481e
Rishi Pratap <rp0258@srmist.edu.in>
2021-10-13 18:43:34 +0530

    add index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>
+            BUY-YANTRA's | Official Website
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            button{
+                padding:5px;
+            }
+            .inter{
+            display: flex;
+            flex-direction: column;
+            padding: 2rem;
+            margin: 0 auto;
+            width:250px;
+            }
+        </style>
+    </head>
+    <body>
+        <center><h1>Seller Buyer common place</h1></center>
+        <br>
+        <div class="inter">
+        <button><a href="buyer.php">Buyer</a></button>&nbsp
+        <button><a href="seller.php">Seller</a></button>
+        </div>
+    </body>
+    <script>
+
+    </script>
+</html>
